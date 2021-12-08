c9680283392740a9d3c895fb085dccfd985cd19f
Adam <a.antoni@wp.pl>
2021-12-08 18:24:47 +0100

    new version of run

--- run.sh
+++ run.sh
@@ -19,5 +19,6 @@ CONVTYPE=${16}
 VARIATIONAL=${17} 
 LOSS=${18} 
 
-srun -p idss-student  python3 -u -m training.py -pathframs $PATH_FRAMS -pathdata $PATH_DATA -pathout $PATH_OUT -batchsize $BATCH_SIZE -adjsize $ADJ_SIZE -numfeatures $NUM_FEATURES -latentdim $LATENT_DIM -nhidden $NHIDDEN -convtype $CONVTYPE -convenc $CONVENC -denseenc $DENSEENC -densedeca $DENSEDECA -convdecx $CONVDECX -densedecx $DENSEDECX -learningrate $LEARNING_RATE -epochs $EPOCHS -variational $VARIATIONAL -loss $LOSS -trainid $SLURM_ARRAY_TASK_ID
+cd framspy
 
+srun -p idss-student python3 -u -m GAE.training.py -pathframs $PATH_FRAMS -pathdata $PATH_DATA -pathout $PATH_OUT -batchsize $BATCH_SIZE -adjsize $ADJ_SIZE -numfeatures $NUM_FEATURES -latentdim $LATENT_DIM -nhidden $NHIDDEN -convtype $CONVTYPE -convenc $CONVENC -denseenc $DENSEENC -densedeca $DENSEDECA -convdecx $CONVDECX -densedecx $DENSEDECX -learningrate $LEARNING_RATE -epochs $EPOCHS -variational $VARIATIONAL -loss $LOSS -trainid $SLURM_ARRAY_TASK_ID
