7a0cece983a287b6788cf70e59b69e51034f82d1
Adam <a.antoni@wp.pl>
2021-12-10 13:28:24 +0100

    removing duplicates

--- framsfiles/reader/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._all import load, loads, parse_value
-
-__all__ = ["load", "loads"]
--- framsfiles/tests/run_tests.cmd
+++
@@ -1,6 +0,0 @@
-pushd ..\..
-echo "Testing framswriter..."
-python -m unittest framsfiles/tests/writer_tests.py
-echo "Testing framsreader..."
-python -m unittest framsfiles/tests/reader_tests.py
-popd
